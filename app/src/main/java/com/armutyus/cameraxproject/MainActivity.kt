06bc0cb0657fa43cca4ae29113024a3d178ce6a4
codegenius2 <lucifer231220@gmail.com>
2022-10-17 23:13:05 +0300

    dependencies & theme

--- app/src/main/java/com/armutyus/cameraxproject/MainActivity.kt
+++ app/src/main/java/com/armutyus/cameraxproject/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.armutyus.cameraxproject
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.sharp.Search
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.armutyus.cameraxproject.ui.theme.CameraXProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +27,67 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CameraXProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "gallery_screen"
                 ) {
-                    Greeting("Android")
+                    composable("gallery_screen") {
+                        //GalleryScreen(navController = navController)
+                    }
+                    composable("camera_screen") {
+                        //CameraScreen(navController = navController)
+                    }
+                    composable("settings_screen") {
+                        //SettingsScreen(navController = navController)
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    val context = LocalContext.current
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { Toast.makeText(context, "Clicked", Toast.LENGTH_LONG).show() }) {
+                Icon(imageVector = Icons.Filled.Add, contentDescription = "Open Camera")
+            }
+        },
+        bottomBar = {
+            BottomAppBar() {
+                IconButton(onClick = { Toast.makeText(context, "Search", Toast.LENGTH_LONG).show() }) {
+                    Icon(imageVector = Icons.Sharp.Search, contentDescription = "Search")
+                }
+            }
+        }
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            TestText(string = "Hello!")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun TestText(string: String) {
+    Column(modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.Center, 
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = string, fontSize = 24.sp, textAlign = TextAlign.Center)
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     CameraXProjectTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
